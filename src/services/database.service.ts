e8d7c4f5de8670ed51612821c12ef3a57c82060d
Joseph Rodman <josephrodman5@gmail.com>
2022-12-04 19:46:13 -0700

    removed unused funcitons

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -316,43 +316,7 @@ class DatabaseService {
         }
     }
 
-    public async getPlayerDetails(external_player_id): Promise<Player>
-    {
-        try {
-            const player = await this.client.player.findFirstOrThrow({
-                where: { external_id: external_player_id },
-            });
 
-            return player;
-        }
-        catch(e)
-        {
-            console.log(e);
-            return null;
-        }
-    }
-
-    public async getTeamNFLGames(external_team_id: number): Promise<NFLGame[]>
-    {
-        try {
-            const games = this.client.nFLGame.findMany({
-                where: {
-                    OR: 
-                    [
-                        { home_team_id: external_team_id },
-                        { away_team_id: external_team_id },
-                    ],
-                },
-            });
-
-            return games;
-        }
-        catch(e)
-        {
-            console.log(e);
-            return null;
-        }
-    }
 
 }
 
