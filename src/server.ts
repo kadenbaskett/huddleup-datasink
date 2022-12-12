733b2eee58e394a25200cf9b6697b335acee33e8
Joseph Rodman <josephrodman5@gmail.com>
2022-12-12 08:34:39 -0700

    added command line args for running the server

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,56 @@
 import App from '@/app';
 import Seed from './seed';
 
-const init = false; // Do we need to fill the DB with NFL stats? (it takes a few minutes to do so)
-const dev = true;
-const app = new App();
-const onlyClearDB = false;
+const args = process.argv.slice(2);
 
-if(dev)
+// Init will add NFL stats to the DB (takes a few mins)
+const initOnly = args.includes('init') && !args.includes('seed');
+const seedOnly = !args.includes('init') && args.includes('seed');
+const initAndSeed = args.includes('init') && args.includes('seed');
+const onlyClearDB = args.includes('onlyClearDB');
+
+const app = new App();
+
+if(process.env.NODE_ENV === 'development')
 {
     const seed = new Seed();
 
     if(onlyClearDB)
     {
+        console.log('clear only');
+        // Removes all of the seeded league data from the db
         seed.clearLeagueStuff();
     }
-    else if(init)
+    else if(initAndSeed)
     {
+        console.log('init and seed');
+        // Updates the db with NFL players, games, teams, etc, and then seeds it with mock league data
         app.initialUpdate().then(() => seed.seedDB());
     }
-    else
+    else if(seedOnly)
     {
+        console.log('seed only');
+        // Seeds the db with mock league data
         seed.seedDB();
     }
+    else if(initOnly)
+    {
+        console.log('init only');
+        // Updates the db with NFL players, games, teams, etc
+        app.initialUpdate();
+    }
+    else 
+    {
+        console.log('Must run with command line args to output the desired behavior. See package.json for a list of available args');
+    }
 }
-else
+else if(process.env.NODE_ENV === 'production')
 {
+    // Updates the db with NFL players, games, teams, etc, and then starts the update loop to keep our DB in sync
     app.initialUpdate().then(() => app.startUpdateLoop());
+}
+else 
+{
+    console.log('Unrecognized node env: ', process.env.NODE_ENV);
 }
 
