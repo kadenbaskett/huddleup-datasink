8b852f69b331425aed86ee25e0334965bbc39898
Justin Perez <u1248103@umail.utah.edu>
2022-12-18 18:57:28 -0500

    cloned data sink

---
+++ src/app.ts
@@ -0,0 +1,290 @@
+import { NFLGame, NFLTeam, Player, Timeframe } from '@prisma/client';
+import { respObj } from '@interfaces/respobj.interface';
+import DatabaseService from '@services/database.service';
+import StatsService from '@services/stats.service';
+
+const hoursToMilliseconds = (hours) => {
+  return hours * 60 * 60 * 1000;
+};
+
+// TODO create a config file
+const config = {
+  timeframe: hoursToMilliseconds(1),
+  teams: hoursToMilliseconds(1),
+  players: hoursToMilliseconds(1),
+  schedule: hoursToMilliseconds(1),
+  gamesInProgress: 60000, // Update once a minute
+};
+
+
+class App {
+
+  stats: StatsService;
+  db: DatabaseService;
+
+  constructor()
+  {
+    this.stats = new StatsService();
+    this.db = new DatabaseService();
+  }
+
+  startUpdateLoop()
+  {
+    setInterval(this.updateTimeframes.bind(this), config.timeframe);
+    setInterval(this.updateTeams.bind(this), config.teams);
+    setInterval(this.updatePlayers.bind(this), config.players);
+    setInterval(this.updateSchedule.bind(this), config.schedule);
+    setInterval(this.updateGameScoresAndPlayerStats.bind(this), config.gamesInProgress);
+  }
+
+  async printDatabase()
+  {
+      const timeframe = await this.db.getTimeframe();
+      console.log('Timeframe: ', timeframe[0]);
+      const teams = await this.db.getNFLTeams();
+      console.log('Teams: ', teams[0]);
+      const schedule = await this.db.getAllNFLGames();
+      console.log('Schedule: ', schedule[0]);
+      const players = await this.db.getPlayers();
+      console.log('Players: ', players[0]);
+      const stats = await this.db.getAllPlayerStats();
+      const cmc = stats.filter((gamelog) => gamelog.external_player_id === 18877);
+      console.log('Stats: ', cmc ? cmc[0] : 'No CMC stats');
+  }
+
+  async clearDB()
+  {
+    console.log('Clearing the database before initial update');
+    await this.db.client.timeframe.deleteMany();
+    await this.db.client.playerGameStats.deleteMany();
+    await this.db.client.nFLGame.deleteMany();
+    await this.db.client.player.deleteMany();
+    await this.db.client.nFLTeam.deleteMany();
+  }
+
+  async initialUpdate()
+  {
+    await this.clearDB();
+
+    console.log('Adding all NFL teams, schedules, and players to the db...');
+    await this.updateTimeframes();
+    await this.updateTeams();
+    await this.updateSchedule();
+    await this.updatePlayers();
+    console.log('Adding all NFL games to the db...');
+    await this.updateCompletedGames();
+    console.log('Updaing game scores and player stats...');
+    await this.updateGameScoresAndPlayerStats();
+
+    await this.printDatabase();
+  }
+
+  async updateTimeframes()
+  {
+    const resp = await this.stats.getTimeframe();
+
+    if(resp.data)
+    {
+      const timeframe = Object(resp.data[0]);
+      await this.db.setTimeframe(Number(timeframe.Season), Number(timeframe.Week));
+    }
+  }
+
+  async updateTeams()
+  {
+    const timeframe: Timeframe = await this.db.getTimeframe();
+
+    if(timeframe)
+    {
+        const resp: respObj = await this.stats.getNFLTeams(timeframe.current_season);
+
+        if(resp.data)
+        {
+          const data = Object(resp.data);
+
+          const teams = data.map(team => {
+              return {
+                external_id: team.TeamID,
+                key: team.Key,
+                city: team.City,
+                name: team.Name,
+                season: timeframe.current_season,
+              };
+            });
+          
+          await this.db.setNFLTeams(teams);
+        }
+    }
+  }
+
+  async updatePlayers()
+  {
+      const resp: respObj = await this.stats.getPlayers();
+
+      if(resp.data)
+      {
+        const data = Object(resp.data);
+
+        const players: Player[] = data.filter(p => p.GlobalTeamID > 0).map(p => {
+            return {
+              external_id: p.PlayerID,
+              first_name: p.FirstName,
+              last_name: p.LastName,
+              status: p.Status,
+              position: p.Position,
+              photo_url: p.PhotoUrl,
+              nfl_team_external_id: p.GlobalTeamID,
+            };
+        });
+
+        await this.db.setPlayers(players);
+      }
+  }
+
+
+  async updateSchedule() {
+
+    const timeframe: Timeframe = await this.db.getTimeframe();
+
+    if(timeframe)
+    {
+        const resp: respObj = await this.stats.getSchedules(timeframe.current_season);
+
+        if(resp.data)
+        {
+          const data = Object(resp.data);
+
+          const games: NFLGame[] = data.filter(g => g.GameKey).map(g => {
+
+              return {
+                external_id: Number(g.GameKey),
+                season: g.Season,
+                week: g.Week,
+                date: new Date(g.Date),
+                away_team_id: g.GlobalAwayTeamID,
+                home_team_id: g.GlobalHomeTeamID,
+                status: g.Status,
+                external_score_id: g.ScoreID,
+              };
+          });
+
+          await this.db.setNFLSchedule(games);
+        }
+    }
+  }
+
+
+  async updateCompletedGames()
+  {
+      const gamesCompleted = await this.db.getCompletedGames();
+
+      if(gamesCompleted.length)
+      {
+        for(const game of gamesCompleted)
+        {
+            const resp: respObj = await this.stats.getBoxScore(game.external_score_id);
+
+            if(resp.data)
+            {
+              const boxScore = Object(resp.data);
+              
+              const game = await this.db.updateScore(Number(boxScore.Score.GameKey), boxScore.Score.homeScore, boxScore.Score.AwayScore);
+
+              for(const pg of boxScore.PlayerGames)
+              {
+                  const playerId = await this.db.externalToInternalPlayer(pg.PlayerID);
+                  const teamId = await this.db.externalToInternalNFLTeam(Number(pg.GlobalTeamID));
+
+                  if(playerId)
+                  {
+                    // Have to use Math.floor to convert floats to Int becuase the data is scrambled
+                    const gameStats = {
+                      external_player_id: pg.PlayerID,
+                      external_game_id: Number(pg.GameKey),
+                      player_id: playerId,
+                      team_id: teamId,
+                      game_id: game.id,
+                      pass_yards: Math.floor(pg.PassingYards),
+                      pass_attempts: Math.floor(pg.PassingAttempts),
+                      completions: Math.floor(pg.PassingCompletions),
+                      pass_td: Math.floor(pg.PassingTouchdowns),
+                      interceptions_thrown: Math.floor(pg.PassingInterceptions),
+                      receptions: Math.floor(pg.Receptions),
+                      rec_yards: Math.floor(pg.ReceivingYards),
+                      targets: Math.floor(pg.ReceivingTargets),
+                      rush_attempts: Math.floor(pg.RushingAttempts),
+                      rush_yards: Math.floor(pg.RushingYards),
+                      rush_td: Math.floor(pg.RushingTouchdowns),
+                      two_point_conversion_passes: Math.floor(pg.TwoPointConversionPasses),
+                      two_point_conversion_runs: Math.floor(pg.TwoPointConversionRuns),
+                      two_point_conversion_receptions: Math.floor(pg.TwoPointConversionReceptions),
+                    };
+
+                    await this.db.updatePlayerGameStats(gameStats);
+                  }
+              }
+            }
+        }
+      }
+  }
+
+  async updateGameScoresAndPlayerStats()
+  {
+      const gamesInProgress = await this.db.getGamesInProgress();
+
+      if(gamesInProgress.length)
+      {
+        for(const game of gamesInProgress)
+        {
+            const resp: respObj = await this.stats.getBoxScore(game.external_score_id);
+
+            if(resp.data)
+            {
+              const boxScore = Object(resp.data);
+              
+              const game = await this.db.updateScore(Number(boxScore.Score.GameKey), boxScore.Score.homeScore, boxScore.Score.AwayScore);
+
+              for(const pg of boxScore.PlayerGames)
+              {
+                  const playerId = await this.db.externalToInternalPlayer(pg.PlayerID);
+                  const teamId = await this.db.externalToInternalNFLTeam(Number(pg.TeamID));
+
+                  if(playerId)
+                  {
+                    // Have to use Math.floor to convert floats to Int becuase the data is scrambled
+                    const gameStats = {
+                      external_player_id: pg.PlayerID,
+                      external_game_id: Number(pg.GameKey),
+                      player_id: playerId,
+                      team_id: teamId,
+                      game_id: game.id,
+                      pass_yards: Math.floor(pg.PassingYards),
+                      pass_attempts: Math.floor(pg.PassingAttempts),
+                      completions: Math.floor(pg.PassingCompletions),
+                      pass_td: Math.floor(pg.PassingTouchdowns),
+                      interceptions_thrown: Math.floor(pg.PassingInterceptions),
+                      receptions: Math.floor(pg.Receptions),
+                      rec_yards: Math.floor(pg.ReceivingYards),
+                      targets: Math.floor(pg.ReceivingTargets),
+                      rush_attempts: Math.floor(pg.RushingAttempts),
+                      rush_yards: Math.floor(pg.RushingYards),
+                      rush_td: Math.floor(pg.RushingTouchdowns),
+                      two_point_conversion_passes: Math.floor(pg.TwoPointConversionPasses),
+                      two_point_conversion_runs: Math.floor(pg.TwoPointConversionRuns),
+                      two_point_conversion_receptions: Math.floor(pg.TwoPointConversionReceptions),
+                    };
+
+                    await this.db.updatePlayerGameStats(gameStats);
+                  }
+                }
+            }
+        }
+      }
+  }
+}
+
+
+
+
+
+export default App;
---
+++ src/services/database.service.ts
@@ -0,0 +1,409 @@
+import { 
+    NFLGame,
+    NFLTeam,
+    Player,
+    PrismaClient, 
+    Timeframe,
+    PlayerGameStats,
+} from '@prisma/client';
+
+class DatabaseService {
+
+    client: PrismaClient;
+
+    constructor()
+    {
+        this.client = new PrismaClient();
+    }
+
+    // ***************** SETTERS ******************
+
+    public async setTimeframe(season: number, week: number): Promise<Timeframe>
+    {
+        try 
+        {
+            const timeframe: Timeframe = await this.client.timeframe.upsert({
+                where: { 
+                    current_season_current_week: { 
+                        current_season: season,
+                        current_week: week,
+                    },
+                 },
+                update: {}, // Don't update if the current season and week match
+                create: {
+                    current_season: season,
+                    current_week: week,
+                },
+            });
+
+            return timeframe;
+        }
+        catch(e) {
+           console.log(e);
+           return null; 
+        }
+    }
+
+    public async setNFLTeams(teams): Promise<NFLTeam[]>
+    {
+        for(const t of teams)
+        {
+            try {
+                await this.client.nFLTeam.upsert({
+                    where: { 
+                        external_id: t.external_id,
+                    },
+                    update: {
+                        external_id: t.external_id,
+                        name: t.name,
+                        key: t.key,
+                        city: t.city,
+                        season: t.season,
+                    },
+                    create: {
+                        external_id: t.external_id,
+                        name: t.name,
+                        key: t.key,
+                        city: t.city,
+                        season: t.season,
+                    },
+                }); 
+            }
+            catch(e)
+            {
+                console.log(e);
+                console.log(t);
+                return null;
+            }
+        } 
+    }
+
+
+    public async setPlayers(players): Promise<Player[]>
+    {
+        for(const p of players)
+        {
+            try {
+                await this.client.player.upsert({
+                    where: { 
+                        external_id: p.external_id,
+                    },
+                    update: {
+                        external_id: p.external_id,
+                        first_name: p.first_name,
+                        last_name: p.last_name,
+                        status: p.status,
+                        position: p.position,
+                        photo_url: p.photo_url,
+                        current_nfl_team_external_id: p.nfl_team_external_id,
+                    },
+                    create: {
+                        external_id: p.external_id,
+                        first_name: p.first_name,
+                        last_name: p.last_name,
+                        status: p.status,
+                        position: p.position,
+                        photo_url: p.photo_url,
+                        current_nfl_team_external_id: p.nfl_team_external_id,
+                    },
+                }); 
+
+            }
+            catch(e)
+            {
+                console.log(e);
+                console.log(p);
+                return null;
+            }
+        } 
+    }
+
+    public async getNFLTeams(): Promise<NFLTeam[]>
+    {
+        try {
+            const teams = this.client.nFLTeam.findMany();
+
+            return teams;
+        }
+        catch(e)
+        {
+            console.log(e);
+            return null;
+        }
+    }
+
+    public async setNFLSchedule(games): Promise<NFLGame[]>
+    {
+        for(const g of games)
+        {
+            try {
+                await this.client.nFLGame.upsert({
+                    where: { 
+                        external_id: g.external_id,
+                    },
+                    update: {
+                        external_id: g.external_id,
+                        season: g.season,
+                        week: g.week,
+                        date: g.date,
+                        away_team_id: g.away_team_id,
+                        home_team_id: g.home_team_id,
+                        status: g.status,
+                        external_score_id: g.external_score_id,
+                    },
+                    create: {
+                        external_id: g.external_id,
+                        season: g.season,
+                        week: g.week,
+                        date: g.date,
+                        away_team_id: g.away_team_id,
+                        home_team_id: g.home_team_id,
+                        status: g.status,
+                        external_score_id: g.external_score_id,
+                        home_score: 0,
+                        away_score: 0,
+                    },
+                }); 
+
+            }
+            catch(e)
+            {
+                console.log(e);
+                console.log(g);
+                return null;
+            }
+        } 
+    }
+
+    public async updateScore(external_game_id: number, home_score: number, away_score: number)
+    {
+        try {
+            const game = await this.client.nFLGame.update({
+                where: { 
+                    external_id: external_game_id,
+                },
+                data: {
+                    home_score: home_score,
+                    away_score: away_score,
+                },
+            }); 
+
+            return game;
+        }
+        catch(e)
+        {
+            console.log(external_game_id, home_score, away_score);
+            console.log(e);
+            return null;
+        }
+    }
+
+    public async updatePlayerGameStats(gameStats)
+    {
+        try {
+            await this.client.playerGameStats.upsert({
+                where: {
+                    external_game_id_external_player_id: {
+                        external_game_id: gameStats.external_game_id,
+                        external_player_id: gameStats.external_player_id,
+                    },
+                },
+                update: {
+                    external_game_id: gameStats.external_game_id,
+                    external_player_id: gameStats.external_player_id,
+                    pass_yards: gameStats.pass_yards,
+                    pass_attempts: gameStats.pass_attempts,
+                    completions: gameStats.completions,
+                    pass_td: gameStats.pass_td,
+                    interceptions_thrown: gameStats.interceptions_thrown,
+                    receptions: gameStats.receptions,
+                    rec_yards: gameStats.rec_yards,
+                    targets: gameStats.targets,
+                    rush_attempts: gameStats.rush_attempts,
+                    rush_yards: gameStats.rush_yards,
+                    rush_td: gameStats.rush_td,
+                    two_point_conversion_passes: gameStats.two_point_conversion_passes,
+                    two_point_conversion_runs: gameStats.two_point_conversion_runs,
+                    two_point_conversion_receptions: gameStats.two_point_conversion_runs,
+                    player_id: gameStats.player_id,
+                    team_id: gameStats.team_id,
+                    game_id: gameStats.game_id,
+                },
+                create: {
+                    external_game_id: gameStats.external_game_id,
+                    external_player_id: gameStats.external_player_id,
+                    pass_yards: gameStats.pass_yards,
+                    pass_attempts: gameStats.pass_attempts,
+                    completions: gameStats.completions,
+                    pass_td: gameStats.pass_td,
+                    interceptions_thrown: gameStats.interceptions_thrown,
+                    receptions: gameStats.receptions,
+                    rec_yards: gameStats.rec_yards,
+                    targets: gameStats.targets,
+                    rush_attempts: gameStats.rush_attempts,
+                    rush_yards: gameStats.rush_yards,
+                    rush_td: gameStats.rush_td,
+                    two_point_conversion_passes: gameStats.two_point_conversion_passes,
+                    two_point_conversion_runs: gameStats.two_point_conversion_runs,
+                    two_point_conversion_receptions: gameStats.two_point_conversion_runs,
+                    player_id: gameStats.player_id,
+                    team_id: gameStats.team_id,
+                    game_id: gameStats.game_id,
+                },
+            });
+        }
+        catch(e)
+        {
+            console.log(gameStats);
+            console.log(e);
+        }
+    }
+
+    // ***************** GETTERS ******************
+
+    public async getPlayers(): Promise<Player[]>
+    {
+        try {
+            return await this.client.player.findMany();
+        }
+        catch(e)
+        {
+            console.log(e);
+            return null;
+        }
+    }
+
+    public async getTimeframe(): Promise<Timeframe>
+    {
+        try {
+            return await this.client.timeframe.findFirst();
+        }
+        catch(e)
+        {
+            console.log(e);
+            return null;
+        }
+    }
+
+
+    public async getAllNFLGames(): Promise<NFLGame[]>
+    {
+        try {
+            return this.client.nFLGame.findMany();
+        }
+        catch(e)
+        {
+            console.log(e);
+            return null;
+        }
+    }
+
+    public async getAllPlayerStats(): Promise<PlayerGameStats[]>
+    {
+        try {
+            return this.client.playerGameStats.findMany();
+        }
+        catch(e)
+        {
+            console.log(e);
+            return null;
+        }
+    }
+
+    public async getGamesInProgress(): Promise<NFLGame[]>
+    {
+        try {
+            const games = this.client.nFLGame.findMany({
+                where: { status: 'InProgress' },
+            });
+
+            return games;
+        }
+        catch(e)
+        {
+            console.log(e);
+            return null;
+        }
+    }
+
+
+    public async getCompletedGames(): Promise<NFLGame[]>
+    {
+        try {
+            const games = this.client.nFLGame.findMany({
+                where: { status: 'Final' },
+            });
+
+            return games;
+        }
+        catch(e)
+        {
+            console.log(e);
+            return null;
+        }
+    }
+
+
+    /*********** HELPER METHODS *************/
+
+    public async externalToInternalPlayer(external_id: number)
+    {
+        try {
+            const player = await this.client.player.findFirstOrThrow({
+                where: {
+                    external_id: external_id,
+                },
+            });
+
+            return player.id;
+        }
+        catch(e)
+        {
+            // console.log(e, external_id);
+            return null;
+        }
+    }
+
+    public async externalToInternalNFLTeam(external_id: number)
+    {
+        try {
+            const team = await this.client.nFLTeam.findFirstOrThrow({
+                where: {
+                    external_id: external_id,
+                },
+            });
+
+            return team.id;
+        }
+        catch(e)
+        {
+            // console.log(e, external_id);
+            return null;
+        }
+    }
+
+
+    public async externalToInternalNFLGame(external_id: number)
+    {
+        try {
+            const game = await this.client.nFLGame.findFirstOrThrow({
+                where: {
+                    external_id: external_id,
+                },
+            });
+
+            return game.id;
+        }
+        catch(e)
+        {
+            // console.log(e, external_id);
+            return null;
+        }
+    }
+
+
+
+
+}
+
+export default DatabaseService;
