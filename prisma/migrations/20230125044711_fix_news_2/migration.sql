dd3da01e9d8fec67eb793962e595c4ed480ea670
Joseph Rodman <josephrodman5@gmail.com>
2023-01-27 17:44:33 -0700

    updated migrations

--- prisma/migrations/20230125044711_fix_news_2/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `externl_team_id2` on the `news` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `news` DROP COLUMN `externl_team_id2`,
-    ADD COLUMN `external_team_id2` INTEGER NULL;
